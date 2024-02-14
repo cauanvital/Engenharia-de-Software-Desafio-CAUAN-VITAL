8d4ec5fabeee0d2e6e0de3678ce6bf080c86f8dc
Cauan Vital <123972992+cauanvital@users.noreply.github.com>
2024-02-13 21:42:26 -0300

    Add files via upload

---
+++ crazy challenge WOW.py
@@ -0,0 +1,63 @@
+# Importing the required extensions to execute the script
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SHEET_ID = "106kadSqmzl8ufnpe1qFTtiYCUgsWRZcskxGydGCq1Fc"
+
+
+# Defines the main function to connect the program with the spreadsheet and update the requested data
+def main():
+    credentials = None
+
+    # Checks if token and credentials json files are configured correctly to grant write access to the spreadsheet
+    if os.path.exists("token.json"):
+        credentials = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            credentials = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(credentials.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=credentials)
+        sheets = service.spreadsheets()
+
+        for row in range(4, 28):
+            exams = sheets.values().get(spreadsheetId=SHEET_ID, range=f"challenge_sheet!C{row}:F{row}").execute().get("values", [])
+            m = (int(exams[0][1]) + int(exams[0][2]) + int(exams[0][3])) / 3
+            print(f"Processing student number {row - 3}")
+
+            if int(exams[0][0]) > 15:
+                new_situation = "Reprovado por Falta"
+            elif m < 50:
+                new_situation = "Reprovado por Nota"
+            elif 70 > m >= 50:
+                new_situation = "Exame Final"
+            else:
+                new_situation = "Aprovado"
+
+            sheets.values().update(spreadsheetId=SHEET_ID, range=f"challenge_sheet!G{row}",
+                                   valueInputOption="USER_ENTERED", body={"values": [[new_situation]]}).execute()
+
+            if new_situation == "Exame Final":
+                naf = round(100 - m + 0.5)
+            else:
+                naf = 0
+
+            sheets.values().update(spreadsheetId=SHEET_ID, range=f"challenge_sheet!H{row}",
+                                   valueInputOption="USER_ENTERED", body={"values": [[naf]]}).execute()
+    except HttpError as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    main()
+
